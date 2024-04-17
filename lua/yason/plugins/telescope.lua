3b056b7170d2fbe915fbfe8e96b3eb130b585960
Jason <jason.d.callaghan@gmail.com>
2024-04-16 18:32:51 -0700

    lsp and telescope changes

--- lua/yason/plugins/telescope.lua
+++ lua/yason/plugins/telescope.lua
@@ -1,34 +1,35 @@
 return {
-	"nvim-telescope/telescope.nvim", tag = "0.1.4",
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.4",
 	-- or                              , branch = "0.1.x",
-	dependencies = { "nvim-lua/plenary.nvim",
-	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make"},
-	"nvim-tree/nvim-web-devicons",
-},
-config = function()
-	local telescope = require("telescope")
-	local actions = require("telescope.actions")
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-	telescope.setup({
-		defaults = {
-			mappings = {
-				i = {
-					["<C-k>"] = actions.move_selection_previous,
-					["<C-j>"] = actions.move_selection_next,
-					["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-				}
-			}
-		}
-	})
+		telescope.setup({
+			defaults = {
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-j>"] = actions.move_selection_next,
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+		})
 
-	telescope.load_extension("fzf")
+		telescope.load_extension("fzf")
 
-	-- set keymaps
-	local keymap = vim.keymap
+		-- set keymaps
+		local keymap = vim.keymap
 
-	keymap.set("n", "<leader>pf", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-	keymap.set("n", "<leader>pg", "<cmd>Telescope git_files<cr>", { desc = "Search files in git repository" })
-	keymap.set("n", "<leader>ps", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-end
+		keymap.set("n", "<leader>pf", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		keymap.set("n", "<leader>pg", "<cmd>Telescope git_files<cr>", { desc = "Search files in git repository" })
+		keymap.set("n", "<leader>ps", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+	end,
 }
-
